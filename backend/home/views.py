383f1997b83c4de5103c6025ded41b243294ffaf
Jourdi Waller <jourdiwaller@gmail.com>
2019-01-16 16:16:31 +0100

    Itegrated Django backed with frontend html + assets

--- backend/home/views.py
+++ backend/home/views.py
@@ -8,7 +8,7 @@ from .models import User
 def top_five(request):
     top_players_list = User.objects.order_by('-top_score')[:5]
     latest_players_list = User.objects.order_by('-date_joined')[:5]
-    return render(request, 'home/home.html', {
+    return render(request, 'home/index.html', {
         'top_players_list': top_players_list,
         'latest_players_list': latest_players_list})
 
