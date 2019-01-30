28bf0158001cf5146c1729678180e1019c09d202
Campo Teddy <teddy.campo@etu.enseeiht.fr>
2019-01-30 10:19:37 +0100

    mysql OK again

---
+++ backend_db/env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/campo/Documents/fullstack/projet-fil-rouge/backend/env/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
