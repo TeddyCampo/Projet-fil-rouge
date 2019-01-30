96d4436bdcf468876c06b944ee2788012a1cf970
Campo Teddy <teddy.campo@etu.enseeiht.fr>
2019-01-30 16:38:47 +0100

    README suite

--- backend/home/views.py
+++ backend/home/views.py
@@ -5,12 +5,10 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import User
 
+
 def top_five(request):
     top_players_list = User.objects.order_by('-top_score')[:5]
     latest_players_list = User.objects.order_by('-date_joined')[:5]
     return render(request, 'home/index.html', {
         'top_players_list': top_players_list,
         'latest_players_list': latest_players_list})
-
-def prompt_question(request):
-        
