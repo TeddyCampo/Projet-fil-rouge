0cd7703a0e0661757fb1ce68a45f060c20ffbec7
Campo Teddy <teddy.campo@etu.enseeiht.fr>
2019-01-30 16:12:55 +0100

    avec donnees sqlite OK

--- jeta_project/accueil/views.py
+++ jeta_project/accueil/views.py
@@ -1,10 +1,18 @@
 from django.shortcuts import render
 from datetime import datetime
+from .models import Player
 
 # Create your views here.
 
 def accueil(request):
     return render(request, 'accueil/index.html')
+
+def top_five(request):
+    top_players_list = Player.objects.order_by('-top_score')[:5]
+    latest_players_list = Player.objects.order_by('-date_joined')[:5]
+    return render(request, 'accueil/index.html', {
+        'top_players_list': top_players_list,
+        'latest_players_list': latest_players_list })
 
 def project(request):
     return render(request, 'accueil/project.html')
