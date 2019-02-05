94a12d19f6af87f3493364e62f40f758a9012439
Poupiman <poupi@vps643779.ovh.net>
2019-02-05 21:01:48 +0100

    Nettoyage du projet et parametrage nginx + gunicorn

--- backend/game/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    path('', views.game_view, name="game-view")
-]
--- jeta_project/jeta_project/settings.py
+++ jeta_project/jeta_project/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'ezguhj3r4yn&dad^#2=c2*urms%a0so7s$d^d*-($s(cs6ry54'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['51.77.231.224', 'egleduc.com']
 
 
 # Application definition
