7791066c0f89c70f0efa56b3070527cde13822fc
Campo Teddy <teddy.campo@etu.enseeiht.fr>
2019-02-09 18:29:58 +0100

    nouvelle suppression du dossier backend

--- backend/game/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-def game_view(request):
-    return render(request, 'game/index.html', content_type="text/html")
-
-# def game_view(request):
-#     return HttpResponse("A test", content_type="text/html")
