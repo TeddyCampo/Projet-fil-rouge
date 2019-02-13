ca718eab5d71b3ba91840a0c0b6d47aaf1e76274
Campo Teddy <teddy.campo@etu.enseeiht.fr>
2019-02-13 10:05:19 +0100

    qlqs nettoyages

--- jeta_project/accueil/views.py
+++ jeta_project/accueil/views.py
@@ -30,9 +30,6 @@ def game(request):
     return render(request, 'accueil/game.html')
 
 # def login(request):
-#     return render(request, 'registration/login.html')
-
-# def login(request):
 #     username = request.POST['username']
 #     password = request.POST['password']
 #     user = authenticate(request, username=username, password=password)
